a0ff6a804189d695ce971871aa395d6c0d69706a
Michael Freyer <michael.freyer@gmail.com>
2016-06-03 17:21:51 +0100

    ran migration to drop mobile

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603154947) do
+ActiveRecord::Schema.define(version: 20160603162039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.integer  "mobile"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
