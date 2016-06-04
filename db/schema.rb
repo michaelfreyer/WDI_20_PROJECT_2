3890e97d29d012e347ea33ea2f5abb2d45eb48a6
Michael Freyer <michael.freyer@gmail.com>
2016-06-04 09:41:22 +0100

    ran migraiton of date task tsak

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604083839) do
+ActiveRecord::Schema.define(version: 20160604084013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160604083839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.date     "date"
   end
 
   create_table "users", force: :cascade do |t|
