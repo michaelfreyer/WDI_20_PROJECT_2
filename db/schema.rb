12bf646037af6cd24d655088c67bf3ae6d392e9a
Michael Freyer <michael.freyer@gmail.com>
2016-06-04 09:40:51 +0100

    created migration to add date with date to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603170312) do
+ActiveRecord::Schema.define(version: 20160604083839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
-    t.string   "date"
     t.string   "time"
     t.string   "category"
     t.datetime "created_at", null: false
