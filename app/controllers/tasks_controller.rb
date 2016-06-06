2e2e334269ff12c86bd52eeec981df310b2d6bde
Michael Freyer <michael.freyer@gmail.com>
2016-06-06 15:33:50 +0100

    go ical link generating and downloading now need to add my data to it

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,8 +7,21 @@ class TasksController < ApplicationController
 def googleCal
   
     
-   redirect_to tasks_url
+    # Create a calendar with an event (standard method)
+    cal = Icalendar::Calendar.new
+    cal.event do |e|
+      e.dtstart     = Icalendar::Values::Date.new('20050428')
+      e.dtend       = Icalendar::Values::Date.new('20050429')
+      e.summary     = "Meeting with the man."
+      e.description = "Have a long lunch meeting and decide nothing..."
+      e.ip_class    = "PRIVATE"
+    end
 
+   cal.publish
+
+
+   render text: cal.to_ical, content_type: 'text/calendar'
+    
 end
 
   def index
