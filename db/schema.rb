6a5b2ed713271aa6e55b058197448f44e9c87b1a
Michael Freyer <michael.freyer@gmail.com>
2016-06-06 11:16:24 +0100

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606101402) do
+ActiveRecord::Schema.define(version: 20160606101546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160606101402) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.date     "date"
+    t.date     "time"
   end
 
   create_table "users", force: :cascade do |t|
