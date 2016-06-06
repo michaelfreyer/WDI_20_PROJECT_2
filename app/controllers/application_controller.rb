5c6e4bc1a049086e29484678a3e588421be56b5e
Michael Freyer <michael.freyer@gmail.com>
2016-06-06 09:39:28 +0100

    installed bootstrap wth woer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  def icon_category(type)
+
+    
+
+  end
+
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
     def configure_permitted_parameters      
@@ -16,4 +24,7 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
       tasks_path
     end
+
+
+
 end
