b1c6911e706cc9c9faccc932baeeac80adc3bebf
Michael Freyer <michael.freyer@gmail.com>
2016-06-07 17:48:57 +0100

    css log in and sign in pages with new background as well as the look fo the index page

--- app/assets/javascripts/aap.js
+++ app/assets/javascripts/aap.js
@@ -2,36 +2,37 @@ $(function() {
 var test = $('.load-in');
 var test2 = $('.load-in-1');
        
+       
 
-       test.mouseenter( 
+       // test.mouseenter( 
 
-        function()
-        {
-          test.addClass('animated pulse')
-        }
-            ).mouseleave( 
+       //  function()
+       //  {
+       //    test.addClass('animated pulse')
+       //  }
+       //      ).mouseleave( 
 
-            function(){
-              test.removeClass('animated pulse')
-            }
-
-
-          );
-
-       test2.mouseenter( 
-
-        function()
-        {
-          test2.addClass('animated pulse')
-        }
-            ).mouseleave( 
-
-            function(){
-              test2.removeClass('animated pulse')
-            }
+       //      function(){
+       //        test.removeClass('animated pulse')
+       //      }
 
 
-          );
+       //    );
+
+       // test2.mouseenter( 
+
+       //  function()
+       //  {
+       //    test2.addClass('animated pulse')
+       //  }
+       //      ).mouseleave( 
+
+       //      function(){
+       //        test2.removeClass('animated pulse')
+       //      }
+
+
+       //    );
 
 
 
