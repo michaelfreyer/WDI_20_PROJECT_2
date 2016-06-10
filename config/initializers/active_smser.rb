85a31f8522bbd64c9ce49aef132bf759ddb30d95
Michael Freyer <michael.freyer@gmail.com>
2016-06-10 14:41:02 +0100

    patching

--- config/initializers/active_smser.rb
+++ config/initializers/active_smser.rb
@@ -5,7 +5,7 @@ if Rails.env.development? || Rails.env.production?
 
   ActionSmser.delivery_options[:delivery_method] = :nexmo
   ActionSmser.delivery_options[:nexmo] = {
-      :username => '7d78a021', :password => "3a25f383faad4fc2"
+      :username => 'username', :password => "password"
   }
 
   # set callback url to nexmo http://localhost:3000/action_smser/delivery_reports/gateway_commit/nexmo
